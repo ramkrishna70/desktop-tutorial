50c40d2099b7567969ef1d6f89326c1ebb7b72ff
Ram Krishna <ramkrishna70@live.com>
2020-05-17 16:56:01 +0530

    chnaging files

--- myprofile/src/app/app-routing.module.ts
+++ myprofile/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Routes, RouterModule , PreloadAllModules} from '@angular/router';
+import { HomeComponent } from '@pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ 
+	{path: '', redirectTo: '/home', pathMatch: 'full'},
+	{ path: 'home', component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- myprofile/src/app/app.module.ts
+++ myprofile/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FooterComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '**', component: HomeComponent }
+    ]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
