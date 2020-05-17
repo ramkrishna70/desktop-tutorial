4f0dcfafe84a3000c1da3875c908cf4545c38da7
Ram Krishna <ramkrishna70@live.com>
2020-05-17 16:56:50 +0530

    Writing changes of myn profile

---
+++ myprofile/src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+ 
+  }
+
+}
